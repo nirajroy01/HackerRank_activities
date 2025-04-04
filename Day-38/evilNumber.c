6fe1897321fa90f0c6e119710a4d0f2bb60bbe3f
Niraj <nkroy95469@gmail.com>
2025-04-04 23:18:15 +0530

    Day-38

---
+++ Day-38/evilNumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int is_evil_number(int n) {
+    // Your code here to count 1s in binary
+    int count = 0;
+    int res = 0;
+    
+    if(n == 0) return 0;
+    while(n!=0){
+        res = n & 1;
+        n = (n >> 1);
+        if(res == 1){
+            count++;
+        }
+    }
+    if(count % 2 == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n); 
+    printf("%d\n", is_evil_number(n));
+    return 0;
+}
